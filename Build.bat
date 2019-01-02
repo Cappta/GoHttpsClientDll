acd606bfba5878f992b8c3b45a29b00182bd447a
Sérgio Fonseca <sergio.fonseca@cappta.com.br>
2019-01-02 16:22:21 -0200

    Removed specific developer environment dependency from build

--- Build.bat
+++ Build.bat
@@ -1,4 +1,3 @@
 go build -buildmode=c-archive GoHttpsClient.go
 gcc -shared -pthread GoHttpsClient.c GoHttpsClient.a -o GoHttpsClient.dll -lWinMM -lntdll -lWS2_32
 del GoHttpsClient.a
-xcopy C:\Users\sergio.fonseca\go\src\github.com\Cappta\GoHttpsClientDll\GoHttpsClient.dll C:\GIT\GoHttpsClientForCSharp\GoHttpsClientForCSharp\GoHttpsClient.dll /Y
